2f9972283ccbc553bf8c815c21eaddd5c7885875
brahmgardner <brahm.gardner@gmail.com>
2016-07-13 12:12:41 -0400

    Travis report (#2)
    * first commit - preliminary working report
    
    * module returns stream not in callback
    
    * switch to khaos model, implement es.map stream
    
    * write tests, update cli
    
    * touch-ups for pull request

---
+++ bin/cli.js
@@ -0,0 +1,86 @@
+#!/usr/bin/env node
+
+/* eslint no-console:0 */
+
+var travisReport = require('../lib/index.js');
+var yargs = require('yargs');
+var yaml = require('js-yaml');
+var EJSON = require('mongodb-extended-json');
+var fs = require('fs');
+
+var options = yargs.usage('Usage: $0 <organization> --token <oauth token> --feature <feature> [options]')
+  .required(1, '*Organization is required*')
+  .option('token', {
+    alias: 't',
+    describe: 'github oauth access token'
+  })
+  .require('token')
+  .option('feature', {
+    describe: 'feature to report on'
+  })
+  .require('token')
+  .require('feature')
+  .option('forked', {
+    describe: 'include forked directories'
+  })
+  .option('out', {
+    alias: 'o',
+    describe: 'write to file instead of stdout',
+    default: null
+  })
+  .option('format', {
+    describe: 'choose format',
+    default: 'json'
+  })
+  .option('grep', {
+    alias: 'g',
+    describe: 'find repos of a certain pattern'
+  })
+  .choices('format', ['json', 'yaml'])
+  .choices('keys', ['name', 'html_url'])
+  .default('forked', false)
+  .help('help')
+  .alias('help', 'h')
+  .argv;
+
+var argv = yargs.argv;
+
+var report = travisReport({
+  org: options._[0],
+  keys: ['name'],
+  token: argv.token,
+  feature: argv.feature
+});
+
+var data = [];
+
+function writeData() {
+  var output = '';
+  if (argv.format === 'yaml') {
+    output = yaml.dump(data);
+  // } else if (argv.format === 'table') {
+  //   output = makeTable(data, argv.keys).toString();
+  } else {
+    output = EJSON.stringify(data, null, 2);
+  }
+
+  if (argv.out) {
+    var dest = fs.createWriteStream(argv.out);
+    dest.write(output);
+    dest.end();
+  } else {
+    console.log(output);
+  }
+}
+
+report.on('data', function(chunk) {
+  data.push(chunk);
+});
+
+report.on('error', function(err) {
+  console.error(err.message);
+});
+
+report.on('end', function() {
+  writeData();
+});
--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,59 @@
 var githubTravisReport = require('../');
 var assert = require('assert');
+var GitHubApi = require('github');
+var sinon = require('sinon');
 
-describe('github-travis-report', function(){
-  it('should work', function(){
+var report = require('../lib/index.js');
+
+function isObject(obj) {
+  return obj instanceof Object;
+}
+
+function hasProperties(obj) {
+  return obj.hasOwnProperty('name')
+    && obj.hasOwnProperty('version');
+}
+
+describe('github-travis-report', function() {
+  before(function(done) {
+    sinon.stub(GitHubApi.prototype, 'authenticate');
+    done();
+  });
+
+  after(function(done) {
+    GitHubApi.prototype.authenticate.restore();
+    done();
+  });
+
+  it('should work', function() {
     assert(githubTravisReport);
+  });
+  it('should return a stream of objects', function() {
+    var str = report({
+      org: 'mongodb-js',
+      feature: 'node_js'
+    });
+    var data = [];
+    str.on('data', function(chunk) {
+      data.push(chunk);
+    });
+
+    str.on('end', function() {
+      assert.isTrue(data.every(isObject));
+    });
+  });
+  it('objects have property name and version', function() {
+    var str = report({
+      org: 'mongodb-js',
+      feature: 'node_js'
+    });
+    var data = [];
+    str.on('data', function(chunk) {
+      data.push(chunk);
+    });
+
+    str.on('end', function() {
+      assert.isTrue(data.every(hasProperties));
+    });
   });
 });
